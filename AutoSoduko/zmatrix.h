85fa65d509266f75e96a6decf6d50a6d46b2e206
razerhell <lzh9407@gmail.com>
2017-03-12 12:53:29 +0800

    add 4 files

---
+++ AutoSoduko/zmatrix.h
@@ -0,0 +1,31 @@
+﻿#ifndef ZMATRIX_H
+#define ZMATRIX_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class ZMatrix
+{
+private:
+	size_t		mRowSize;
+	size_t		mColSize;
+protected:
+	vector<T>	mData;
+
+public:
+	// 提供三种形式的构造函数：无参，全参，拷贝构造函数
+	ZMatrix(vector<T> data = vector<T>(), size_t row = 0, size_t col = 0);
+	ZMatrix(const ZMatrix &other);
+
+	const vector<T>&	getData() const;
+	vector<T>			getData();
+
+	// 重载 = 操作符
+	ZMatrix<T>& operator = (const ZMatrix& other);
+};
+
+#include "zmatrix.inl"
+
+#endif
